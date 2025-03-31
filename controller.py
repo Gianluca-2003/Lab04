5e28e91cb30d80bd226cd9812e15d91262e18a3b
Gianluca Garruso <s311880@studenti.polito.it>
2025-03-31 13:45:28 +0200

    Completed

--- controller.py
+++ controller.py
@@ -7,10 +7,37 @@ class SpellChecker:
     def __init__(self, view):
         self._multiDic = md.MultiDictionary()
         self._view = view
+        self._myLanguage = None
+        self._mytxtIn = None
+        self._myModality = None
+
+
+    def check_language(self, e):
+        self._myLanguage = self._view._dd1.value
+        if self._myLanguage == "":
+            print("Lingua non inserita")
+            return
+        else:
+            self._view.txtOut.controls.append(ft.Text(f"Lingua inserita: {self._myLanguage}"))
+            self._view.update()
+            print("Lingua inserita")
+
+
+    def check_modality(self, e):
+        self._myModality = self._view._dd2.value
+        if self._myModality == "":
+            print("Modalita non inserita")
+            return
+        else:
+            self._view.txtOut.controls.append(ft.Text(f"Modalità inserita: {self._myModality}"))
+            self._view.update()
+            print("Modalita inserita")
+
+
+
 
     def handleSentence(self, txtIn, language, modality):
         txtIn = replaceChars(txtIn.lower())
-
         words = txtIn.split()
         paroleErrate = " - "
 
@@ -22,6 +49,8 @@ class SpellChecker:
                     if not parola.corretta:
                         paroleErrate = paroleErrate + str(parola) + " - "
                 t2 = time.time()
+                self._view.txtOut.controls.append(ft.Text(f"Parole errate: {paroleErrate} e tempo: {t2 - t1} ", color="green"))
+                self._view.update()
                 return paroleErrate, t2 - t1
 
             case "Linear":
@@ -31,6 +60,8 @@ class SpellChecker:
                     if not parola.corretta:
                         paroleErrate = paroleErrate + str(parola) + " "
                 t2 = time.time()
+                self._view.txtOut.controls.append(ft.Text(f"Parole errate: {paroleErrate} \nTempo: {t2 - t1} ", color="green"))
+                self._view.update()
                 return paroleErrate, t2 - t1
 
             case "Dichotomic":
@@ -40,9 +71,34 @@ class SpellChecker:
                     if not parola.corretta:
                         paroleErrate = paroleErrate + str(parola) + " - "
                 t2 = time.time()
+                self._view.txtOut.controls.append(ft.Text(f"Parole errate: {paroleErrate} e tempo: {t2 - t1} ", color="green"))
+                self._view.update()
                 return paroleErrate, t2 - t1
             case _:
                 return None
+
+    def handleSpellCheck(self, e):
+        if self._myLanguage is None:
+            self._view.txtOut.controls.append(ft.Text(f"Devi scegliere una lingua", color="red"))
+            self._view.update()
+            return
+        if self._myModality is None:
+            self._view.txtOut.controls.append(ft.Text(f"Devi scegliere una modalità", color="red"))
+            self._view.update()
+            return
+        self._mytxtIn = self._view.txtIn.value
+        if self._mytxtIn == "":
+            self._view.txtOut.controls.append(ft.Text(f"Devi inserire una frase", color="red"))
+            self._view.update()
+            return
+        self._view.txtOut.controls.append(ft.Text(f"Frase Inserita: {self._mytxtIn}"))
+        self._view.update()
+        print("Frase Inserita")
+        self.handleSentence(self._mytxtIn, self._myLanguage, self._myModality)
+
+
+
+
 
 
     def printMenu(self):
--- view.py
+++ view.py
@@ -3,6 +3,7 @@ import flet as ft
 class View(object):
     def __init__(self, page: ft.Page):
         # Page
+        self.btnSpellChech = None
         self.page = page
         self.page.title = "TdP 2024 - Lab 04 - SpellChecker ++"
         self.page.horizontal_alignment = 'CENTER'
@@ -12,6 +13,11 @@ class View(object):
         # UI elements
         self.__title = None
         self.__theme_switch = None
+        self._language = None
+        self._dd1 = None
+        self.txtOut = None
+        self.txtIn = None
+        self._dd2 = None
 
         # define the UI elements and populate the page
 
@@ -25,17 +31,56 @@ class View(object):
             ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
                    alignment=ft.MainAxisAlignment.START)
         )
+        self.txtOut = ft.ListView()
+
+        self._dd1 = ft.Dropdown(label="Select language",
+                         width= self.page.width,
+                         options=[ft.dropdown.Option("english"),
+                                  ft.dropdown.Option("italian"),
+                                  ft.dropdown.Option("spanish")],
+                         on_change = self.__controller.check_language)
+
+        self.page.add(self._dd1)
+
+        self._dd2 = ft.Dropdown(
+            width=self.page.width*0.20,
+            label="Search modality",
+            options=[
+            ft.dropdown.Option("Linear"),
+            ft.dropdown.Option("Dichotomic"),
+            ft.dropdown.Option("Default"),
+            ],
+            on_change = self.__controller.check_modality)
+
+        self.txtIn = ft.TextField(value="", label="Add your sentence here",width= self.page.width*0.6)
+
+        self.btnSpellChech = ft.ElevatedButton(text="Spell Check",
+                                               color="blue",
+                                               on_click=self.__controller.handleSpellCheck,
+                                               width= self.page.width*0.15)
+
+        row3 = ft.Row(
+            controls=[self._dd2, self.txtIn, self.btnSpellChech],
+            #alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        )
+        self.page.add(row3)
+
+        self.page.add(self.txtOut)
+
 
         # Add your stuff here
 
-        self.page.add([])
+
 
         self.page.update()
 
     def update(self):
         self.page.update()
+
+
     def setController(self, controller):
         self.__controller = controller
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
